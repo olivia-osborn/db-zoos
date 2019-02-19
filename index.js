aa0dd07a459148de16001fa17def013509a94ea5
Olivia Osborn <olioz95@gmail.com>
2019-02-19 17:10:07 -0500

    finished all CRUD operators

--- index.js
+++ index.js
@@ -43,6 +43,58 @@ server.get('/api/zoos/:id', async (req, res) => {
 
 })
 
+// POST:
+server.post('/api/zoos', async (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({error: 'please enter a name!'})
+  }
+  try {
+    const [id] = await db('zoos').insert(req.body);
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+    res.status(201).json({zoo})
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+// DELETE:
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id})
+      .del();
+      if (count > 0) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ error: "zoo not found"})
+      }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//PUT: 
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id})
+      .update(req.body)
+
+    if (count > 0) {
+      const zoo = await db('zoos')
+        .where({id: req.params.id})
+        .first();
+
+        res.status(200).json(zoo)
+    } else {
+      res.status(404).json({error: "zoo not found"})
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
