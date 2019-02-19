c3abd64abfbc0131eb91f819f634020ea83923c4
Olivia Osborn <olioz95@gmail.com>
2019-02-19 16:40:51 -0500

    now able to GET all zoos and zoo by id

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/zoos.db3',
+  },
+  useNullAsDefault: true,
+}
+const db = knex(knexConfig)
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// GET zoos:
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
+//GET /api/zoos/:id:
+server.get('/api/zoos/:id', async (req, res) => {
+    try {
+      const zoo = await db('zoos')
+        .where({id: req.params.id})
+        .first();
+        if (!zoo) {
+          res.status(404).json({error: "Zoo with that ID could not be found"})
+        } else {
+          res.status(200).json(zoo);
+        }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+
+})
 
 const port = 3300;
 server.listen(port, function() {
